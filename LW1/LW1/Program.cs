d4c246348ef51a08610fe860fe7de6bf345813ad
BEASTDK7 <72353569+BEASTDK7@users.noreply.github.com>
2023-02-15 21:43:57 +0300

    Add files via upload

---
+++ LW1/LW1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace LW1
+{
+    class Program
+    {
+        delegate void MyFirstDelegate(int pParam);
+        delegate int Summ(int a, int v);
+        delegate string ConverterFromFloat(float D);
+        delegate int[] ToVector(int a, int b, int c);
+        delegate string GetAString();
+
+
+        static void Main(string[] args)
+        {
+
+            string spaceNoise = "1o12i12u1y213fguaboba090kllkfo87k";
+            string alienSignal = "aboba";
+
+
+            //Лямбда-выражение
+            bool extraterrestrialIntelligence() =>
+                spaceNoise.Contains(alienSignal);
+
+            //Делегаты Action<Func<a> b, c> и Лямбда-выражение
+            Action<Func<bool>, int, int> resPrint = (resFunc, basis, power) =>
+            {
+                string res = "";
+                if (resFunc())
+                {
+                    res = "OMG! Alien Signal! Light Years To Target: " + (Math.Pow(basis, power)).ToString();    
+                }
+                else
+                {
+                    res = "White Noise. No Signals Of Extraterrestrial Intelligence Found.";
+                }
+
+                Console.WriteLine(res);
+            };
+
+            resPrint(extraterrestrialIntelligence, 887, 13);
+        }
+    }
+}
